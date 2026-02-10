ac160e0219c6c8686aff3d792baef53986d8c969
Akshat <akshatbansal217@gmail.com>
2026-02-10 18:02:22 +0530

    Langchain models

---
+++ 3.EmbeddedModels/embedding_openai_docs.py
@@ -0,0 +1,20 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+
+embedding = OpenAIEmbeddings(model = "text-embedding-3-large", dimensions=32)
+
+documents = [
+    "Delhi is the capital of india",
+    "Kolkata is the capital of west bengal",
+    "Paris is the capital of france"
+    
+]
+
+
+result = embedding.embad_documents(documents)
+print(str(result))
